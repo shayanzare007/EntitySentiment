dd564a960ce9aa18fa1fdf46856da5e283539979
Youssef Ahres <youssefahres@gmail.com>
2015-05-13 21:48:17 -0700

    added text_to_indices

---
+++ text_to_ind.py
@@ -0,0 +1,26 @@
+
+
+def text_to_ind(input_file,output_file):
+    vocab = {}
+    count = 0
+    with open(input_file) as f:
+        for i,line in enumerate(f):
+            line = line.split(' ')
+            for j,word in enumerate(line):
+                word = word.rstrip()
+                if word not in vocab:
+                    vocab[word] = count
+                    count += 1
+    output = open(output_file,"w")
+    with open(input_file) as f:
+        for i,line in enumerate(f):
+            line = line.split(' ')
+            for j,word in enumerate(line):
+                word = word.rstrip()
+                if j==len(line)-1:
+                    output.write(str(vocab[word]))
+                else:
+                    output.write(str(vocab[word])+',')
+            output.write('\n')
+
+text_to_ind('text_recu.txt','testing_parser')
